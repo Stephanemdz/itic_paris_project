78c35ee8b06cf0ca10a02cb1ef3eaadcfe194ba5
stephane mdz <stephanemodzinu@gmail.com>
2024-11-07 17:57:24 +0100

    modif database

--- sql/data.sql
+++ sql/data.sql
@@ -1,29 +1,30 @@
+CREATE DATABASE itic_paris;
 CREATE TABLE user(
 id_user INT PRIMARY KEY AUTO_INCREMENT, 
 is_admin BOOLEAN DEFAULT 0, 
-is_trainer BOOLEAN, 
+is_trainer BOOLEAN DEFAULT 0, 
 firstname VARCHAR(100) NOT NULL, 
 lastname VARCHAR(100)NOT NULL, 
 birthdate DATE NOT NULL, 
 email VARCHAR(100) NOT NULL, 
 password VARCHAR(100) NOT NULL,
-created_at DATE, 
-updated_at DATE,
-deleted_at DATE);
+created_at DATETIME NOT NULL, 
+updated_at DATETIME NOT NULL,
+deleted_at DATETIME);
 
 CREATE TABLE contract (
 id_contract INT PRIMARY KEY AUTO_INCREMENT,
 type VARCHAR(100) NOT NULL,
 start_date DATE NOT NULL,
 file VARCHAR(100) NOT NULL,
-created_at DATE,
-updated_at DATE,
-deleted_at DATE);
+created_at DATETIME NOT NULL,
+updated_at DATETIME NOT NULL,
+deleted_at DATETIME);
 
 CREATE TABLE mission(
 id_mission INT PRIMARY KEY AUTO_INCREMENT, 
 title VARCHAR(100) NOT NULL,
 description TEXT NOT NULL, 
-created_at DATE, 
-updated_at DATE,
-deleted_at DATE);
+created_at DATETIME NOT NULL, 
+updated_at DATETIME NOT NULL,
+deleted_at DATETIME);
